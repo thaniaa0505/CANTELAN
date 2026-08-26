ddb86dc23af24c5c742507f81b5251c7d64e4c29
thaniaa0505 <thaniaudreygs@gmail.com>
2026-08-26 09:24:03 +0700

    Create app_onboarding2.dart

---
+++ app_onboarding2.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  AppImages._();
+  static const String imagePath = "assets/images";
+  static const String iconPath = "assets/icons";
+  static const String dragonBall = "$imagePath/Dragon_Ball.jpg";
+  static const String fb = "$imagePath/Fb.jpg";
+}
